4c0e78f26aa8366557b916d48ef308d01b0c629f
shingo-osnnlsyd <shingo.wakabayashi@icloud.com>
2022-12-18 10:41:08 +0900

    GH-4: ログイン機能を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { createTheme, ThemeProvider } from "@mui/material"
+import { createContext, useEffect, useState } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import BookAppBar from "./components/BookAppBar"
+import { User } from "./interfaces/User"
+import { getCurrentUser } from "./lib/api/auth"
+import SignIn from "./pages/auth/SignIn"
 import Ballance from "./pages/Ballance"
 import Budgets from "./pages/Budgets"
 import Items from "./pages/Items"
@@ -12,17 +16,52 @@ const theme = createTheme({
   }
 })
 
+export const AuthContext = createContext({} as {
+  isSignedIn: boolean
+  setIsSignedIn: React.Dispatch<React.SetStateAction<boolean>>
+  currentUser: User | undefined
+  setCurrentUser: React.Dispatch<React.SetStateAction<User | undefined>>
+})
+
 const App = () => {
+  const [isSignedIn, setIsSignedIn] = useState<boolean>(false);
+  const [currentUser, setCurrentUser] = useState<User | undefined>(undefined);
+
+  const handleGetCurrentUser = async () => {
+    try {
+      const res = await getCurrentUser()
+      if (res?.data.isLogin === true) {
+        setIsSignedIn(true)
+        setCurrentUser(res?.data.data)
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  useEffect(() => {
+    handleGetCurrentUser()
+  }, [setCurrentUser])
+
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
-        <BookAppBar />
-        <Routes>
-          <Route path="/" element={<Ballance />} />
-          <Route path="/ballance" element={<Ballance />} />
-          <Route path="/budgets" element={<Budgets />} />
-          <Route path="/items" element={<Items />} />
-        </Routes>
+        <AuthContext.Provider value={{ isSignedIn, setIsSignedIn, currentUser, setCurrentUser}}>
+          <BookAppBar />
+          <Routes>
+          <Route path="/signin" element={<SignIn />} />
+            {isSignedIn ? (
+              <>
+                <Route path="/" element={<Ballance />} />
+                <Route path="/ballance" element={<Ballance />} />
+                <Route path="/budgets" element={<Budgets />} />
+                <Route path="/items" element={<Items />} />
+              </>
+            ) : (
+              <Route path="/" element={<></>} />
+            )}
+          </Routes>
+        </AuthContext.Provider>
       </ThemeProvider>
     </BrowserRouter>
   )
